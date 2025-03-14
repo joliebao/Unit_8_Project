5f1e694ee2f62e04ef49f1750511047c664a2398
BT_2S14_04 <@>
2025-03-14 11:03:45 -0400

    Part 2 started; thinking of ways to solve it!
        // count the amount of turns each path has, put most number of turns in method in the maze solver
        // other method is to set a checkpoint at a specific point and get it back there

---
+++ src/turnCount.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class turnCount {
+    // count the amount of turns each path has, put that method in the maze solver
+    // other method is to set a checkpoint at a specific point and get it back there
+    private String[][] maze;
+    public turnCount (String[][] m){
+        maze = m;
+    }
+}
